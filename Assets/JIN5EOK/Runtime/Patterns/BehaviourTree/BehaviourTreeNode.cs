685aaa0d158cd265be8aacb74b021ae480d16c1f
JIN5EOK <baejinseok98@gmail.com>
2025-03-09 19:10:21 +0900

    ✨feat: Add BehaviourTree

---
+++ Assets/JIN5EOK/Runtime/Patterns/BehaviourTree/BehaviourTreeNode.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace Jin5eok.Patterns
+{
+    public interface IBehaviourTreeNode
+    {
+        public IBehaviourTreeNode AddChild(IBehaviourTreeNode child);
+        public bool Execute();
+    }
+
+    public abstract class BehaviourTreeNode : IBehaviourTreeNode 
+    {
+        protected List<IBehaviourTreeNode> Children { get; set; }
+        
+        public IBehaviourTreeNode AddChild(IBehaviourTreeNode child)
+        {
+            if (Children != null)
+            {
+                Children.Add(child);    
+            }
+            return this;
+        }
+
+        public abstract bool Execute();
+    }
+}
---
+++ Assets/JIN5EOK/Runtime/Patterns/BehaviourTree/ControlNodes.cs
@@ -0,0 +1,55 @@
+namespace Jin5eok.Patterns
+{
+    public class SequencerNode : BehaviourTreeNode
+    {
+        public override bool Execute()
+        {
+            foreach (var child in Children)
+            {
+                if (child.Execute() == false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    
+    public class ParallelSequencerNode : BehaviourTreeNode
+    {
+        public override bool Execute()
+        {
+            var isSucceedAll = true;
+            foreach (var child in Children)
+            {
+                isSucceedAll &= child.Execute();
+            }
+            return isSucceedAll;
+        }
+    }
+    
+    public class SelectorNode : BehaviourTreeNode
+    {
+        public override bool Execute() 
+        {
+            foreach (var child in Children)
+            {
+                if (child.Execute() == true) return true; 
+            }
+            return false;
+        }
+    }
+    
+    public class ParallelSelectorNode : BehaviourTreeNode
+    {
+        public override bool Execute()
+        {
+            var isSucceed = false;
+            foreach (var child in Children)
+            {
+                isSucceed |= child.Execute();
+            }
+            return isSucceed;
+        }
+    }
+}
