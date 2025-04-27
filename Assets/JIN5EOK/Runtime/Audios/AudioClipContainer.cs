56b66862800f1c130202d3695c77b3f3f17107ab
JIN5EOK <baejinseok98@gmail.com>
2025-04-27 18:17:09 +0900

    ♻️refactor: Refactor AudioClipContainer
    - Add memory release in ClearCache
    - Support sync and async AudioClip loading

--- Assets/JIN5EOK/Runtime/Audios/AudioClipContainer.cs
+++ Assets/JIN5EOK/Runtime/Audios/AudioClipContainer.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 #if USE_ADDRESSABLES
+using System.Threading.Tasks;
 using UnityEngine.AddressableAssets;
 #endif
 
@@ -8,46 +9,123 @@ namespace Jin5eok.Audios
 {
     public class AudioClipContainer
     {
-        private static readonly Dictionary<string, AudioClip> _audioClipMap = new();
-
-        public static AudioClip GetFromResources(string resourcePath, bool isCache = true)
+        private static readonly Dictionary<string, AudioClip> _resourcesAudioClips = new();
+        
+        public static AudioClip GetFromResources(string resourcePath)
         {
             AudioClip clip = null;
-            if (_audioClipMap.TryGetValue(resourcePath, out clip) == false)
+            if (_resourcesAudioClips.TryGetValue(resourcePath, out clip) == false)
             {
                 clip = Resources.Load<AudioClip>(resourcePath);
-                if (clip != null && isCache == true)
+                if (clip != null)
                 {
-                    _audioClipMap.Add(resourcePath, clip);
+                    _resourcesAudioClips.Add(resourcePath, clip);
                 }
             }
             return clip;
         }
-
-#if USE_ADDRESSABLES
-        public static AudioClip GetFromAddressables(string address, bool isCache = true)
+        
+        public async static Task<AudioClip> GetFromResourcesAsync(string resourcePath)
         {
             AudioClip clip = null;
-            if (_audioClipMap.TryGetValue(address, out clip) == false)
+            if (_resourcesAudioClips.TryGetValue(resourcePath, out clip) == false)
             {
-                clip = Addressables.LoadAssetAsync<AudioClip>(address).WaitForCompletion();
-                if (clip != null && isCache == true)
+                var request = Resources.LoadAsync<AudioClip>(resourcePath);
+                while (request.isDone == false)
                 {
-                    _audioClipMap.Add(address, clip);
+                    await Task.Yield();
+                }
+                clip = request.asset as AudioClip;
+                if (clip != null)
+                {
+                    _resourcesAudioClips.Add(resourcePath, clip);
                 }
             }
-            return _audioClipMap[address];
+            return clip;
         }
+        
+        
+#if USE_ADDRESSABLES
+
 #endif
         
-        public static int GetCachedCount()
+        public static int GetCachedResourcesCount()
         {
-            return _audioClipMap.Count;
+            return _resourcesAudioClips.Count;
         }
         
-        public static void ClearCachedClips()
+        public static void ClearResourcesCache()
         {
-            _audioClipMap.Clear();
+            foreach (var clip in _resourcesAudioClips.Values)
+            {
+                Resources.UnloadAsset(clip);
+            }
+            _resourcesAudioClips.Clear();
         }
+        
+#if USE_ADDRESSABLES
+        private static readonly Dictionary<string, AudioClip> _addressablesAudioClips = new();
+
+        public static AudioClip GetFromAddressables(string address)
+        {
+            AudioClip clip = null;
+            if (_resourcesAudioClips.TryGetValue(address, out clip) == false)
+            {
+                var handle = Addressables.LoadAssetAsync<AudioClip>(address);
+                clip = handle.WaitForCompletion();
+                if (clip != null)
+                {
+                    _resourcesAudioClips.Add(address, clip);
+                }
+                else
+                {
+                    Addressables.Release(handle);
+                }
+            }
+            else
+            {
+                clip = _resourcesAudioClips[address];
+            }
+            return clip;
+        }
+        
+        public async static Task<AudioClip> GetFromAddressablesAsync(string address)
+        {
+            AudioClip clip = null;
+            if (_resourcesAudioClips.TryGetValue(address, out clip) == false)
+            {
+                var handle = Addressables.LoadAssetAsync<AudioClip>(address);
+                clip = await handle.Task;
+                if (clip != null)
+                {
+                    _resourcesAudioClips.Add(address, clip);
+                }
+                else
+                {
+                    Addressables.Release(handle);
+                }
+            }
+            else
+            {
+                clip = _resourcesAudioClips[address];
+            }
+            return clip;
+        }
+        
+        public static int GetCachedAddressablesCount()
+        {
+            return _addressablesAudioClips.Count;
+        }
+
+        public static void ClearAddressablesCache()
+        {
+            foreach (var clip in _addressablesAudioClips.Values)
+            {
+                Addressables.Release(clip);
+            }
+            _addressablesAudioClips.Clear();
+        }
+#endif
+
     }
 }
