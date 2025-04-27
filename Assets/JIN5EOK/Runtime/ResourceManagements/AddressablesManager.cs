adbac4cfa1970e0bfbd1f7568d4a4cf936a46f1f
JIN5EOK <baejinseok98@gmail.com>
2025-04-28 03:11:23 +0900

    ✨feat: Replace AudioClipContainer into AddressablesManager

---
+++ Assets/JIN5EOK/Runtime/ResourceManagements/AddressablesManager.cs
@@ -0,0 +1,104 @@
+#if USE_ADDRESSABLES
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Jin5eok.ResourceManagements
+{
+    public class AddressablesManager
+    {
+        private static class AsyncOperationHandleMap<T> where T : Object
+        {
+            public static Dictionary<AsyncOperationHandle<T>, string> HandleToAddressMap { get; private set; } = new ();
+            public static Dictionary<string, AsyncOperationHandle<T>> AddressToHandleMap { get; private set; } = new ();
+        }
+        
+        public static T LoadAsset<T>(string address) where T : Object => LoadAssetAsync<T>(address).WaitForCompletion();
+        
+        public static AsyncOperationHandle<T> LoadAssetAsync<T>(string address) where T : Object
+        {
+            if (AsyncOperationHandleMap<T>.AddressToHandleMap.TryGetValue(address, out var handle) == false || handle.IsValid() == false)
+            {
+                if (handle.IsValid() == false)
+                {
+                    AsyncOperationHandleMap<T>.AddressToHandleMap.Remove(address);
+                    AsyncOperationHandleMap<T>.HandleToAddressMap.Remove(handle);
+                }
+                
+                handle = Addressables.LoadAssetAsync<T>(address);
+                AsyncOperationHandleMap<T>.AddressToHandleMap.Add(address, handle);
+                AsyncOperationHandleMap<T>.HandleToAddressMap.Add(handle, address);
+            }
+            else
+            {
+                handle = AsyncOperationHandleMap<T>.AddressToHandleMap[address];
+            }
+            return handle;
+        }
+
+        public static bool ReleaseAsset<T>(string address) where T : Object
+        {
+            if (AsyncOperationHandleMap<T>.AddressToHandleMap.TryGetValue(address, out var handle) == true)
+            {
+                if (handle.IsValid() == true)
+                {
+                    Addressables.Release(handle);    
+                }
+                AsyncOperationHandleMap<T>.AddressToHandleMap.Remove(address);
+                AsyncOperationHandleMap<T>.HandleToAddressMap.Remove(handle);
+                return true;
+            }
+            
+            return false;
+        }
+        
+        public static bool ReleaseAsset<T>(AsyncOperationHandle<T> handle) where T : Object
+        {
+            if (AsyncOperationHandleMap<T>.HandleToAddressMap.TryGetValue(handle, out var address) == true)
+            {
+                if (handle.IsValid() == true)
+                {
+                    Addressables.Release(handle);    
+                }
+                
+                AsyncOperationHandleMap<T>.AddressToHandleMap.Remove(address);
+                AsyncOperationHandleMap<T>.HandleToAddressMap.Remove(handle);
+                return true;
+            }
+            return false;
+        }
+        
+        public static void ReleaseAssetAll<T>() where T : Object
+        {
+            foreach (var handle in AsyncOperationHandleMap<T>.AddressToHandleMap.Values)
+            {
+                if (handle.IsValid() == true)
+                {
+                    Addressables.Release(handle);    
+                }
+            }
+            AsyncOperationHandleMap<T>.AddressToHandleMap.Clear();
+            AsyncOperationHandleMap<T>.HandleToAddressMap.Clear();
+        }
+        
+        public static int GetCount<T>() where T : Object
+        {
+            return AsyncOperationHandleMap<T>.AddressToHandleMap.Count;
+        }
+        
+        public static void ClearInvalidRequest<T>() where T : Object
+        {
+            foreach (var handle in AsyncOperationHandleMap<T>.AddressToHandleMap.Values)
+            {
+                if (handle.IsValid() == false)
+                {
+                    var address = AsyncOperationHandleMap<T>.HandleToAddressMap[handle];
+                    AsyncOperationHandleMap<T>.AddressToHandleMap.Remove(address);
+                    AsyncOperationHandleMap<T>.HandleToAddressMap.Remove(handle);
+                }
+            }
+        }
+    }
+}
+#endif
