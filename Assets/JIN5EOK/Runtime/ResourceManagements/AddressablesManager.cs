f9e58bf05ae27aaead5e762068d088f56a075881
JIN5EOK <baejinseok98@gmail.com>
2025-05-02 22:25:07 +0900

    ♻️refactor: AddressablesManager : Hide AsyncOperationHandle, support UniTask, Awaitable, Coroutine

--- Assets/JIN5EOK/Runtime/ResourceManagements/AddressablesManager.cs
+++ Assets/JIN5EOK/Runtime/ResourceManagements/AddressablesManager.cs
@@ -1,25 +1,79 @@
 #if USE_ADDRESSABLES
-using System.Collections.Concurrent;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using Jin5eok.Patterns;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using Object = UnityEngine.Object;
 
 namespace Jin5eok.ResourceManagements
 {
-    public class AddressablesManager
+    public class AddressablesManager : MonoSingleton<AddressablesManager>
     {
         private static class AsyncOperationHandleMap<T> where T : Object
         {
-            public static Dictionary<AsyncOperationHandle<T>, string> HandleToAddressMap { get; private set; } = new ();
             public static Dictionary<string, AsyncOperationHandle<T>> AddressToHandleMap { get; private set; } = new ();
         }
         private static object _lock = new ();
         
-        public static T LoadAsset<T>(string address) where T : Object => LoadAssetAsync<T>(address).WaitForCompletion();
+        public static T LoadSync<T>(string address) where T : Object => GetHandle<T>(address).WaitForCompletion();
         
-        public static AsyncOperationHandle<T> LoadAssetAsync<T>(string address) where T : Object
+        public static void LoadAsyncCoroutine<T>(string address, Action<T> onResult) where T : Object 
+        {
+            var handle = GetHandle<T>(address);
+            if (handle.IsValid() == false)
+            {
+                onResult?.Invoke(null);
+            }
+            else if (handle.Status == AsyncOperationStatus.Succeeded)
+            {
+                onResult?.Invoke(handle.Result);
+            }
+            else
+            {
+                Instance.StartCoroutine(LoadAsyncCoroutineInternal(handle, onResult));
+            }
+        }
+        
+        private static IEnumerator LoadAsyncCoroutineInternal<T>(AsyncOperationHandle<T> handle, Action<T> onResult) where T : Object
+        {
+            yield return new WaitUntil(() => handle.IsDone);
+            onResult?.Invoke(handle.Result);
+        }
+        
+#if USE_UNITASK
+        public static async UniTask<T> LoadAsyncUniTask<T>(string address) where T : Object
+        {
+            await UniTask.SwitchToMainThread();
+            var handle = GetHandle<T>(address);
+            await handle.Task;
+            return handle.Result;
+        }
+#endif
+        
+#if USE_AWAITABLE
+        public static async Awaitable<T> LoadAsyncAwaitable<T>(string address) where T : Object
+        {
+            await Awaitable.MainThreadAsync();
+            var handle = GetHandle<T>(address);
+            await handle.Task;
+            return handle.Result;
+        }
+#endif
+        
+        public static async Task<T> LoadAsyncTask<T>(string address) where T : Object
+        {
+            var handle = GetHandle<T>(address);
+            await handle.Task;
+            return handle.Result;
+        }
+        
+        private static AsyncOperationHandle<T> GetHandle<T>(string address) where T : Object
         {
             lock (_lock)
             {
@@ -31,73 +85,29 @@ namespace Jin5eok.ResourceManagements
                     }
                     else
                     {
-                        AsyncOperationHandleMap<T>.AddressToHandleMap.Remove(address);
-                        AsyncOperationHandleMap<T>.HandleToAddressMap.Remove(handle);    
+                        AsyncOperationHandleMap<T>.AddressToHandleMap.Remove(address);    
                     }
                 }
                 
                 handle = Addressables.LoadAssetAsync<T>(address);
-                handle.Destroyed += _ =>
-                {
-                    lock (_lock)
-                    {
-                        if (AsyncOperationHandleMap<T>.AddressToHandleMap.TryGetValue(address, out var currentHandle) == true
-                            && currentHandle.Equals(handle) == true)
-                        {
-                            AsyncOperationHandleMap<T>.AddressToHandleMap.Remove(address);
-                            AsyncOperationHandleMap<T>.HandleToAddressMap.Remove(handle);    
-                        }
-                    }
-                };
                 AsyncOperationHandleMap<T>.AddressToHandleMap.Add(address, handle);
-                AsyncOperationHandleMap<T>.HandleToAddressMap.Add(handle, address);
                 
                 return handle;
             }
         }
         
-        public static bool ReleaseAsset<T>(string address) where T : Object
+        public static bool Release<T>(string address) where T : Object
         {
             lock (_lock)
             {
                 if (AsyncOperationHandleMap<T>.AddressToHandleMap.TryGetValue(address, out var handle) == true)
                 {
-                    return ReleaseAssetInternal(handle);
-                }
-                return false;
-            }
-        }
-        
-        public static bool ReleaseAsset<T>(AsyncOperationHandle<T> handle) where T : Object
-        {
-            return ReleaseAssetInternal(handle);
-        }
-
-        public static void ReleaseAssetAll<T>() where T : Object
-        {
-            lock (_lock)
-            {
-                var handles = AsyncOperationHandleMap<T>.AddressToHandleMap.Values.ToArray();
-                foreach (var handle in handles)
-                {
-                    ReleaseAssetInternal(handle);
-                }
-            }
-        }
-        
-        private static bool ReleaseAssetInternal<T>(AsyncOperationHandle<T> handle) where T : Object
-        {
-            lock (_lock)
-            {
-                if (AsyncOperationHandleMap<T>.HandleToAddressMap.TryGetValue(handle, out var address) == true)
-                {
-                    if (handle.IsValid() && handle.Status == AsyncOperationStatus.Succeeded && handle.Result != null)
+                    if (handle.IsValid() == true && handle.Status == AsyncOperationStatus.Succeeded && handle.Result != null)
                     {
                         Addressables.Release(handle);    
                     }
-                
+                    
                     AsyncOperationHandleMap<T>.AddressToHandleMap.Remove(address);
-                    AsyncOperationHandleMap<T>.HandleToAddressMap.Remove(handle);
                     return true;
                 }
 
@@ -105,7 +115,19 @@ namespace Jin5eok.ResourceManagements
             }
         }
         
-        public static int GetHandleCount<T>() where T : Object
+        public static void ReleaseAll<T>() where T : Object
+        {
+            lock (_lock)
+            {
+                var addresses = AsyncOperationHandleMap<T>.AddressToHandleMap.Keys.ToArray();
+                foreach (var address in addresses)
+                {
+                    Release<T>(address);
+                }
+            }
+        }
+        
+        public static int GetLoadedCount<T>() where T : Object
         {
             lock (_lock)
             {
