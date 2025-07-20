a057c25765cacc8ef06e576a932042ba1cad3b92
JIN5EOK <baejinseok98@gmail.com>
2025-07-20 18:05:07 +0900

    ♻️refactor: Refactor AudioPlayers, Remove AudioModel

--- Assets/JIN5EOK/Runtime/Audios/AudioManager.cs
+++ Assets/JIN5EOK/Runtime/Audios/AudioManager.cs
@@ -1,8 +1,7 @@
-using System;
 using System.Collections.Generic;
-using Jin5eok.Helper;
 using Jin5eok.Patterns;
 using UnityEngine;
+using UnityEngine.Audio;
 #if USE_ADDRESSABLES
 #endif
 
@@ -10,57 +9,37 @@ namespace Jin5eok.Audios
 {
     public class AudioManager : MonoSingleton<AudioManager>
     {
-        public AudioModel GlobalAudioModel { get; private set; } = new AudioModel();
-        private Dictionary<string, AudioModel> _audioModels = new ();
-        private Dictionary<string, OneShotAudioPlayer> _oneShotAudioPlayers = new ();
+        private Dictionary<AudioMixerGroup, AudioPlayer> _oneShotAudioPlayers = new ();
         
-        public bool TryAddAudioType(string key)
+        public AudioPlayer InstantiateAudioPlayer(AudioClip audioClip, AudioMixerGroup audioMixerGroup = null)
         {
-            if (_audioModels.ContainsKey(key) || _oneShotAudioPlayers.ContainsKey(key))
-                return false;
-            
-            var audioModel = new AudioModel();
-            _audioModels.Add(key, audioModel);
-            
-            var oneShotPlayerGameObject = new GameObject($"{nameof(OneShotAudioPlayer)}:{key}");
-            oneShotPlayerGameObject.transform.SetParent(transform);
-                
-            var oneShotPlayer = oneShotPlayerGameObject.AddComponent<OneShotAudioPlayer>();
-            oneShotPlayer.Initialize(_audioModels[key], GlobalAudioModel);
-            _oneShotAudioPlayers.Add(key, oneShotPlayer);
-
-            return true;
-        }
-        
-        public AudioPlayer InstantiateAudioPlayer(AudioClip audioClip, string audioType)
-        {
-            if (_audioModels.ContainsKey(audioType) == false)
-                return null;
-            
-            var playerGameObject = new GameObject($"{nameof(AudioPlayer)}:{audioType}");
+            var playerGameObject = new GameObject($"{nameof(AudioPlayer)}:{audioMixerGroup.name}");
             playerGameObject.transform.SetParent(transform);
             
             var playerInstance = playerGameObject.AddComponent<AudioPlayer>();
-            playerInstance.Initialize(audioClip, _audioModels[audioType], GlobalAudioModel);
+            
+            playerInstance.AudioSource.clip = audioClip;
+            playerInstance.AudioSource.outputAudioMixerGroup = audioMixerGroup;
+            
             return playerInstance;
         }
         
-        public AudioPlayResult PlayOneShot(AudioClip sfxClip, string audioType)
+        public AudioPlayResult PlayOneShot(AudioClip audioClip, AudioMixerGroup audioMixerGroup = null)
         {
-            if (_oneShotAudioPlayers.ContainsKey(audioType) == true)
+            if (_oneShotAudioPlayers.ContainsKey(audioMixerGroup) == false)
             {
-                return _oneShotAudioPlayers[audioType].Play(sfxClip);    
+                var oneShotPlayerGameObject = new GameObject($"GlobalOneShotAudioPlayer:{audioMixerGroup?.name ?? "NoMixer"}");
+                oneShotPlayerGameObject.transform.SetParent(transform);
+                
+                var oneShotPlayer = oneShotPlayerGameObject.AddComponent<AudioPlayer>();
+                
+                oneShotPlayer.AudioSource.clip = audioClip;
+                oneShotPlayer.AudioSource.outputAudioMixerGroup = audioMixerGroup;
+                
+                _oneShotAudioPlayers.Add(audioMixerGroup, oneShotPlayer);
             }
-            return AudioPlayResult.GetFailedResult();
-        }
-
-        public AudioModel GetAudioModel(string key)
-        {
-            if (_audioModels.TryGetValue(key, out var model))
-            {
-                return model;
-            }
-            return null;
+            
+            return _oneShotAudioPlayers[audioMixerGroup].PlayOneShot(audioClip);
         }
     }   
 }
--- Assets/JIN5EOK/Runtime/Audios/AudioPlayer.cs
+++ Assets/JIN5EOK/Runtime/Audios/AudioPlayer.cs
@@ -1,189 +1,61 @@
-using System.Collections;
+using System;
 using UnityEngine;
 
 namespace Jin5eok.Audios
 {
     public class AudioPlayer : MonoBehaviour
     {
-        public AudioClip AudioClip
-        {
-            get => _audioSource.clip;
-            set => _audioSource.clip = value;
-        }
-        public AudioModel PlayerAudioModel { get; } = new AudioModel();
-        public bool Loop
-        {
-            get => _audioSource.loop;
-            set => _audioSource.loop = value;
-        }
-        public bool IsPlaying => _audioSource.isPlaying;
-        public float PlayTime => _audioSource.time;
-        public float Length => _audioSource.clip.length;
+        public event Action OnPlay;
         
-        private AudioModel _baseAudioModel;
-        private AudioModel _globalAudioModel;
-        private AudioSource _audioSource;
-        private Coroutine _fadeVolumeCoroutine;
-        private Coroutine _fadePitchCoroutine;
-        private bool _isInit;
-        
-        private enum ScaleType
-        {
-            Volume,
-            Pitch,
-        }
-        
-        public void Initialize(AudioClip audioClip, AudioModel baseAudioModel, AudioModel globalAudioModel)
-        {
-            if (_isInit == true)
-                return;
+        /// <summary>
+        /// Avoid using play, pause, and stop whenever possible!
+        /// </summary>
+        public AudioSource AudioSource { get; private set; }
 
-            _isInit = true;
-            
-            if (_audioSource == null)
+        private void Awake()
+        {
+            if (AudioSource != null)
             {
-                _audioSource = gameObject.AddComponent<AudioSource>();    
+                AudioSource = gameObject.AddComponent<AudioSource>();    
             }
-            
-            _audioSource.clip = audioClip;
-            _baseAudioModel = baseAudioModel;
-            _globalAudioModel = globalAudioModel;
-            AddEvent();
-            OnAudioModelsChanged();
-        }
-
-        private void AddEvent()
-        {
-            if (_globalAudioModel != null)
-            {
-                _globalAudioModel.OnVolumeChanged += OnVolumeChanged;
-                _globalAudioModel.OnMuteChanged += OnMuteChanged;
-                _globalAudioModel.OnPitchChanged += OnPitchChanged;    
-            }
-
-            if (_baseAudioModel != null)
-            {
-                _baseAudioModel.OnVolumeChanged += OnVolumeChanged;
-                _baseAudioModel.OnMuteChanged += OnMuteChanged;
-                _baseAudioModel.OnPitchChanged += OnPitchChanged;    
-            }
-            
-            PlayerAudioModel.OnVolumeChanged += OnVolumeChanged;
-            PlayerAudioModel.OnMuteChanged += OnMuteChanged;
-            PlayerAudioModel.OnPitchChanged += OnPitchChanged;
-        }
-
-        private void OnVolumeChanged(float volume)
-        {
-            OnAudioModelsChanged();
-        }
-        
-        private void OnPitchChanged(float pitch)
-        {
-            OnAudioModelsChanged();
-        }
-        
-        private void OnMuteChanged(bool mute)
-        {
-            OnAudioModelsChanged();
         }
         
         public AudioPlayResult Play()
         {
-            if (AudioClip == null)
+            try
             {
+                AudioSource.Play();
+                return AudioPlayResult.GetSucceedResult(AudioSource.clip);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
                 return AudioPlayResult.GetFailedResult();
             }
-            
-            _audioSource.Play();
-            return AudioPlayResult.GetSucceedResult(AudioClip);
+        }
+
+        public AudioPlayResult PlayOneShot(AudioClip audioClip)
+        {
+            try
+            {
+                AudioSource.PlayOneShot(audioClip);
+                return AudioPlayResult.GetSucceedResult(audioClip);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                return AudioPlayResult.GetFailedResult();
+            }
         }
         
         public void Pause()
         {
-            _audioSource.Pause();
+            AudioSource.Pause();
         }
         
         public void Stop()
         {
-            _audioSource.Stop();
-        }
-
-        public void FadeVolume(float from, float to, float duration)
-        {
-            if (_fadeVolumeCoroutine != null)
-            {
-                StopCoroutine(_fadeVolumeCoroutine);
-            }
-            _fadeVolumeCoroutine = StartCoroutine(FadeScaleCoroutine(from, to, duration, ScaleType.Volume));
-        }
-        
-        public void FadePitch(float from, float to, float duration)
-        {
-            if (_fadePitchCoroutine != null)
-            {
-                StopCoroutine(_fadePitchCoroutine);
-            }
-            _fadePitchCoroutine = StartCoroutine(FadeScaleCoroutine(from, to, duration, ScaleType.Pitch));
-        }
-        
-        private IEnumerator FadeScaleCoroutine(float from, float to, float duration, ScaleType scaleType)
-        {
-            var time = 0.0f;
-            while (time < duration)
-            {
-                time += Time.deltaTime;
-                ScaleChange(Mathf.Lerp(from, to, time / duration), scaleType);
-                yield return null;
-            }
-            ScaleChange(to, scaleType);
-
-            void ScaleChange(float scale, ScaleType typeOfScale)
-            {
-                switch (typeOfScale)
-                {
-                    case ScaleType.Volume:
-                        PlayerAudioModel.Volume = scale;
-                        break;
-                    case ScaleType.Pitch:
-                        PlayerAudioModel.Pitch = scale;
-                        break;
-                }
-            }
-        }
-        
-        private void OnAudioModelsChanged()
-        {
-            _audioSource.mute = PlayerAudioModel.Mute || _baseAudioModel.Mute || _globalAudioModel.Mute;
-            _audioSource.volume = Mathf.Clamp01(PlayerAudioModel.Volume * _baseAudioModel.Volume * _globalAudioModel.Volume);
-            _audioSource.pitch  = Mathf.Clamp(PlayerAudioModel.Pitch  * _baseAudioModel.Pitch  * _globalAudioModel.Pitch, 0.1f, 3f);
-        }
-        
-        private void OnDestroy()
-        {
-            Stop();
-            RemoveVolumeChangeEvent();
-        }
-
-        private void RemoveVolumeChangeEvent()
-        {
-            if (_globalAudioModel != null)
-            {
-                _globalAudioModel.OnVolumeChanged -= OnVolumeChanged;
-                _globalAudioModel.OnMuteChanged -= OnMuteChanged;
-                _globalAudioModel.OnPitchChanged -= OnPitchChanged;    
-            }
-            
-            if (_baseAudioModel != null)
-            {
-                _baseAudioModel.OnVolumeChanged -= OnVolumeChanged;
-                _baseAudioModel.OnMuteChanged -= OnMuteChanged;
-                _baseAudioModel.OnPitchChanged -= OnPitchChanged;    
-            }
-            
-            PlayerAudioModel.OnVolumeChanged -= OnVolumeChanged;
-            PlayerAudioModel.OnMuteChanged -= OnMuteChanged;
-            PlayerAudioModel.OnPitchChanged -= OnPitchChanged;
+            AudioSource.Stop();
         }
     }
 }
