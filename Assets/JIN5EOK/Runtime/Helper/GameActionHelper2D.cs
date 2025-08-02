eb34cd94cc40be0446985383af6e958ce92b6b3e
JIN5EOK <baejinseok98@gmail.com>
2025-08-02 11:23:25 +0900

    💄style: remove useless code

--- Assets/JIN5EOK/Runtime/Helper/GameActionHelper2D.cs
+++ Assets/JIN5EOK/Runtime/Helper/GameActionHelper2D.cs
@@ -4,26 +4,6 @@ namespace Jin5eok.Helper
 {
     public class GameActionHelper2D
     {
-        public static void Move(GameObject target, Vector2 vel)
-        {
-            target.transform.Translate(vel * Time.deltaTime);
-        }
-        
-        public static void MoveX(GameObject target, float vel)
-        {
-            Move(target, new Vector2(vel, 0));
-        }
-        
-        public static void MoveY(GameObject target, float vel)
-        {
-            Move(target, new Vector2(0, vel));
-        }
-
-        public static void MoveToPoint(GameObject target, Vector2 to, float speed)
-        {
-            Move(target, GetDirection(target.transform.position, to) * speed);
-        }
-        
         public static bool IsGrounded(Vector2 boxSize, Vector2 point, LayerMask groundLayer, float castDistance = 0.1f)
         {
             return Physics2D.BoxCast(point, boxSize, 0, Vector2.down, castDistance, groundLayer);
@@ -37,11 +17,6 @@ namespace Jin5eok.Helper
             return IsGrounded(boxSize, point, groundLayer, castDistance);
         }
         
-        public static void Jump(Rigidbody2D rigidbody2D, float power)
-        {
-            rigidbody2D.AddForce(Vector2.up * power);
-        }
-
         public static Vector2 GetDirection(Vector2 from, Vector2 to)
         {
             return (to - from).normalized;
