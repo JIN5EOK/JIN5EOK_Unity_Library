fcd758bb8e77756861a32cf142a1f416547b9887
JIN5EOK <baejinseok98@gmail.com>
2025-08-02 16:21:08 +0900

    ♻️refactor: Reduce code duplication

--- Assets/JIN5EOK/Runtime/Helper/UnityWebRequestHelper.cs
+++ Assets/JIN5EOK/Runtime/Helper/UnityWebRequestHelper.cs
@@ -13,91 +13,95 @@ namespace Jin5eok
     {
         public static void Get(string url, Action<string> onSuccess, Action<UnityWebRequestException> onError = null)
         {
-            CoroutineRunner.Instance.StartCoroutine(GetRoutine(url, onSuccess, onError));
+            CoroutineRunner.Instance.StartCoroutine(RequestRoutine(UnityWebRequest.Get(url), onSuccess, onError, r => r.downloadHandler.text));
         }
-        
-        private static IEnumerator GetRoutine(string url, Action<string> onSuccess, Action<UnityWebRequestException> onError = null)
-        {
-            using UnityWebRequest request = UnityWebRequest.Get(url);
-            yield return request.SendWebRequest();
-
-            if (request.result == UnityWebRequest.Result.Success)
-            {
-                onSuccess?.Invoke(request.downloadHandler.text);
-            }
-            else
-            {
-                onError?.Invoke(new UnityWebRequestException(request));
-            }
-        }
-        
-        public static async Task<string> GetAsync(string url)
-        {
-            using UnityWebRequest request = UnityWebRequest.Get(url);
-            await request.SendWebRequest(); 
-            return request.downloadHandler.text;
-        }
-        
-#if USE_UNITASK
-        public static async UniTask<string> GetUniTaskAsync(string url)
-        {
-            using UnityWebRequest request = UnityWebRequest.Get(url);
-            await request.SendWebRequest(); 
-            return request.downloadHandler.text;
-        }
-#endif
-
-#if USE_AWAITABLE
-        public static async Awaitable<string> GetAwaitableAsync(string url)
-        {
-            using UnityWebRequest request = UnityWebRequest.Get(url);
-            await request.SendWebRequest(); 
-            return request.downloadHandler.text;
-        }
-#endif
         
         public static void GetTexture(string url, Action<Texture2D> onSuccess, Action<UnityWebRequestException> onError = null)
         {
-            CoroutineRunner.Instance.StartCoroutine(GetTextureRoutine(url, onSuccess, onError));
+            CoroutineRunner.Instance.StartCoroutine(RequestRoutine(UnityWebRequestTexture.GetTexture(url), onSuccess, onError, DownloadHandlerTexture.GetContent));
         }
         
-        private static IEnumerator GetTextureRoutine(string url, Action<Texture2D> onSuccess, Action<UnityWebRequestException> onError = null)
+        private static IEnumerator RequestRoutine<T>(UnityWebRequest request, Action<T> onSuccess, Action<UnityWebRequestException> onError, Func<UnityWebRequest, T> contentExtractor)
         {
-            using UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
-            yield return request.SendWebRequest();
+            using (request)
+            {
+                yield return request.SendWebRequest();
 
-            if (request.result == UnityWebRequest.Result.Success)
-            {
-                onSuccess?.Invoke(DownloadHandlerTexture.GetContent(request));
-            }
-            else
-            {
-                onError?.Invoke(new UnityWebRequestException(request));
+                if (request.result == UnityWebRequest.Result.Success)
+                {
+                    onSuccess?.Invoke(contentExtractor(request));
+                }
+                else
+                {
+                    onError?.Invoke(new UnityWebRequestException(request));
+                }
             }
         }
         
-        public static async Task<Texture2D> GetTextureAsync(string url)
+        public static Task<string> GetAsync(string url)
         {
-            using UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
-            await request.SendWebRequest(); 
-            return DownloadHandlerTexture.GetContent(request);
+            return RequestAsync(UnityWebRequest.Get(url), r => r.downloadHandler.text);
+        }
+        
+        public static Task<Texture2D> GetTextureAsync(string url)
+        {
+            return RequestAsync(UnityWebRequestTexture.GetTexture(url), DownloadHandlerTexture.GetContent);
+        }
+        
+        private static async Task<T> RequestAsync<T>(UnityWebRequest request, Func<UnityWebRequest, T> contentExtractor)
+        {
+            using (request)
+            {
+                await request.SendWebRequest();
+                if (request.result != UnityWebRequest.Result.Success) 
+                    throw new UnityWebRequestException(request);
+                return contentExtractor(request);
+            }
         }
         
 #if USE_UNITASK
-        public static async UniTask<Texture2D> GetTextureUniTaskAsync(string url)
+        public static UniTask<string> GetUniTaskAsync(string url)
         {
-            using UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
-            await request.SendWebRequest(); 
-            return DownloadHandlerTexture.GetContent(request);
+            return RequestUniTaskAsync(UnityWebRequest.Get(url), r => r.downloadHandler.text);
+        }
+        
+        public static UniTask<Texture2D> GetTextureUniTaskAsync(string url)
+        {
+            return RequestUniTaskAsync(UnityWebRequestTexture.GetTexture(url), DownloadHandlerTexture.GetContent);
+        }
+        
+        private static async UniTask<T> RequestUniTaskAsync<T>(UnityWebRequest request, Func<UnityWebRequest, T> contentExtractor)
+        {
+            using (request)
+            {
+                await request.SendWebRequest();
+                if (request.result != UnityWebRequest.Result.Success) 
+                    throw new UnityWebRequestException(request);
+                return contentExtractor(request);
+            }
         }
 #endif
-        
+
 #if USE_AWAITABLE
-        public static async Awaitable<Texture2D> GetTextureAwaitableAsync(string url)
+        public static Awaitable<string> GetAwaitableAsync(string url)
         {
-            using UnityWebRequest request = UnityWebRequest.Get(url);
-            await request.SendWebRequest(); 
-            return DownloadHandlerTexture.GetContent(request);
+            return RequestAwaitableAsync(UnityWebRequest.Get(url), r => r.downloadHandler.text);
+        }
+        
+        public static Awaitable<Texture2D> GetTextureAwaitableAsync(string url)
+        {
+            return RequestAwaitableAsync(UnityWebRequestTexture.GetTexture(url), DownloadHandlerTexture.GetContent);
+        }
+        
+        private static async Awaitable<T> RequestAwaitableAsync<T>(UnityWebRequest request, Func<UnityWebRequest, T> contentExtractor)
+        {
+            using (request)
+            {
+                await request.SendWebRequest();
+                if (request.result != UnityWebRequest.Result.Success)
+                    throw new UnityWebRequestException(request);
+                return contentExtractor(request);
+            }
         }
 #endif
     }
