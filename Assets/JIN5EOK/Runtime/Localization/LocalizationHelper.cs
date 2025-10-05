e598e32cefca110adbee000f6caa2923bbad4951
JIN5EOK <baejinseok98@gmail.com>
2025-10-05 16:54:48 +0900

    ✨feat: change some funcions

--- Assets/JIN5EOK/Runtime/Localization/LocalizationHelper.cs
+++ Assets/JIN5EOK/Runtime/Localization/LocalizationHelper.cs
@@ -6,45 +6,31 @@ using UnityEngine.Localization.SmartFormat.PersistentVariables;
 
 namespace Jin5eok
 {
-        
     public static class LocalizationHelper
     {
-        // Value를 인자로 받는 생성자가 없어 확장 메서드로 대체함 (생성 + 값 지정을 한줄로 쓰기 위해)
-        public static Variable<T> SetValue<T>(this Variable<T> variable, T value)
-        {
-            variable.Value = value;
-            return variable;
-        }
-        
         public static void SetStringReferenceAndRefresh(this LocalizeStringEvent localizedStringEvent, LocalizedString localizedString)
         {
             localizedStringEvent.StringReference = localizedString;
             localizedStringEvent.RefreshString();
         }
-
+        
         // smart string argument 추가, 넘버링 기반일 경우 사용
-        public static void AddArgumentAndRefresh(this LocalizedString localizedString, params object[] arguments)
+        public static void SetArgumentAndRefresh(this LocalizedString localizedString, params object[] arguments)
         {
-            if (localizedString.Arguments == null)
-            {
-                localizedString.Arguments = arguments;
-            }
-            else
-            {
-                foreach (var argument in arguments)
-                {
-                    localizedString.Arguments.Add(argument);    
-                }    
-            }
-            
+            localizedString.Arguments = arguments;
             localizedString.RefreshString();
         }
 
         // smart string argument 추가, 넘버링 기반이 아닌 Key 기반일 경우 사용
-        public static void AddKeyVariablePairAndRefresh(this LocalizedString localizedString, params (string key, IVariable value)[] arguments)
+        public static void SetKeyVariablePairAndRefresh(this LocalizedString localizedString, params (string key, IVariable value)[] arguments)
         {
             foreach (var argument in arguments)
             {
+                if (localizedString.ContainsKey(argument.key))
+                {
+                    localizedString.Remove(argument.key);
+                }
+                
                 localizedString.Add(argument.key, argument.value);
             }
             
