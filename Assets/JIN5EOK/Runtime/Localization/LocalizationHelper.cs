3da70cfa9abab4a4f7f164153c7024e525489442
JIN5EOK <baejinseok98@gmail.com>
2025-10-06 14:21:37 +0900

    💄style: Gemini code review

--- Assets/JIN5EOK/Runtime/Localization/LocalizationHelper.cs
+++ Assets/JIN5EOK/Runtime/Localization/LocalizationHelper.cs
@@ -1,4 +1,5 @@
 #if USE_LOCALIZATION // 로컬라이제이션 임포트시에만 사용
+using System.Linq;
 using UnityEngine.Localization;
 using UnityEngine.Localization.Components;
 using UnityEngine.Localization.Settings;
@@ -24,14 +25,9 @@ namespace Jin5eok
         // smart string argument 추가, 넘버링 기반이 아닌 Key 기반일 경우 사용
         public static void SetKeyVariablePairAndRefresh(this LocalizedString localizedString, params (string key, IVariable value)[] arguments)
         {
-            foreach (var argument in arguments)
+            foreach (var (key, value) in arguments)
             {
-                if (localizedString.ContainsKey(argument.key))
-                {
-                    localizedString.Remove(argument.key);
-                }
-                
-                localizedString.Add(argument.key, argument.value);
+                localizedString[key] = value;
             }
             
             localizedString.RefreshString();
@@ -40,15 +36,9 @@ namespace Jin5eok
         // 문자열 코드 -> 로케일 반환
         public static Locale GetLocaleByCode(string localeCode)
         {
-            var locales = LocalizationSettings.AvailableLocales.Locales;
-            foreach (var locale in locales)
-            {
-                if (locale.Identifier.Code == localeCode)
-                {
-                    return locale;
-                }
-            }
-            return null;
+            if (string.IsNullOrEmpty(localeCode)) return null;
+
+            return LocalizationSettings.AvailableLocales.Locales.FirstOrDefault(locale => locale.Identifier.Code == localeCode);
         }
     }
 }
